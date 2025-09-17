ff2290d26cba2a95501218cf38d2248154ecb98a
Sean Parker <seanryanparker@gmail.com>
2025-09-16 23:44:05 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -756,8 +756,8 @@ with right:
     GOAL_X = 89.0
     GOAL_HALF_THICK = 0.167 / 2  # ~2 in
 
-    # Instead of clipping at 14.5 ft, allow up to ~34 ft
-    GOAL_Y_EXTENT = 34.0
+    # Instead of clipping at 14.5 ft, allow up to ~36 ft
+    GOAL_Y_EXTENT = 36.0
 
     for gx in (-GOAL_X, GOAL_X):
         fig.add_shape(
